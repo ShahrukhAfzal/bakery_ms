b42932a1c13b33630c991c25de726e00d8bbfe7a
shahrukh <shoaibtayyab121@gmail.com>
2020-10-31 23:46:05 +0530

    added authentication permission

--- bakery_management/user/views.py
+++ bakery_management/user/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from user.serializers import UserSerializer
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,4 +10,4 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-
+    permission_classes = (permissions.IsAuthenticated, )
