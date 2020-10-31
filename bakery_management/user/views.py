abfc979e37c3ac4c6cc9b8bdc60d9b48aead7c14
shahrukh <shoaibtayyab121@gmail.com>
2020-10-31 23:21:43 +0530

    added viewset, serializer for user

--- bakery_management/user/views.py
+++ bakery_management/user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from user.serializers import UserSerializer
 
-# Create your views here.
+from rest_framework import viewsets
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
