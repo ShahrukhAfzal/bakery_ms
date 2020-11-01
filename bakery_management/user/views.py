1d2a3cedf9df2467606d3b11288a10e95cc82936
shahrukh <shoaibtayyab121@gmail.com>
2020-11-01 16:13:21 +0530

    fixes

--- bakery_management/user/views.py
+++ bakery_management/user/views.py
@@ -30,14 +30,13 @@ class RegisterAPI(generics.GenericAPIView):
 
 
 class LoginAPI(KnoxLoginView):
-	"""
-	User can login through this api
-	"""
-    permission_classes = (permissions.AllowAny,)
+    """"""
+    permission_classes = (permissions.AllowAny, )
 
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         login(request, user)
+
         return super(LoginAPI, self).post(request, format=None)
