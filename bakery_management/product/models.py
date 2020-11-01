880d181ee491ea780ac9bc190a4a7f9b2e19d8e1
shahrukh <shoaibtayyab121@gmail.com>
2020-11-02 00:22:44 +0530

    model for product and ingredients

--- bakery_management/product/models.py
+++ bakery_management/product/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    cost_price = models.FloatField()
+    selling_price = models.FloatField()
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return "{} = ${}".format(self.name, self.selling_price)
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class ProductIngredient(models.Model):
+    product = models.ForeignKey(Product, related_name='products',
+                on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredients, related_name='products',
+                on_delete=models.SET_NULL, blank=True, null=True)
+    quantity_percent = models.FloatField()
