26484178744ece5975cfd17bd49d04ecf942461b
shahrukh <shoaibtayyab121@gmail.com>
2020-11-03 18:04:07 +0530

    working on product creation

--- bakery_management/product/serializers.py
+++ bakery_management/product/serializers.py
@@ -1,14 +1,6 @@
 from rest_framework import serializers
 
-from product.models import Product, Ingredients
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'description', 'cost_price', 'selling_price',
-                    'quantity'
-                )
+from product.models import Product, Ingredients, ProductIngredient
 
 
 class IngredientsSerializer(serializers.ModelSerializer):
@@ -16,3 +8,30 @@ class IngredientsSerializer(serializers.ModelSerializer):
         model = Ingredients
         fields = ('id', 'name', 'description')
 
+
+class ProductIngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductIngredient
+        fields = ('ingredient', 'quantity_percent')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    ingredients_details = ProductIngredientSerializer(many=True, required=False)
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'cost_price', 'selling_price',
+                    'quantity', 'ingredients_details'
+                )
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients_details')
+        instance = super().create(validated_data)
+
+        for ingredient in ingredients:
+            ProductIngredient.objects.create(
+                ingredient=ingredient['ingredient'],product=instance,
+                quantity_percent=ingredient['quantity_percent'])
+
+        return instance
