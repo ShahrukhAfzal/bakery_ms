c8e72a7075871876d82bd7509f4001abb88bf3ab
shahrukh <shoaibtayyab121@gmail.com>
2020-11-03 08:44:07 +0530

    Only admin can add/edit/update product/ingredients

--- bakery_management/product/views.py
+++ bakery_management/product/views.py
@@ -2,21 +2,25 @@ from rest_framework import viewsets
 
 from product.serializers import ProductSerializer, IngredientsSerializer
 from product.models import Product, Ingredients
+from user.permissions import CustomPermission
 
 
 class IngredientsViewSet(viewsets.ModelViewSet):
 
-    queryset = Ingredients.objects.all()
+    permission_classes = (CustomPermission,)
+    queryset = Ingredients.objects.all().order_by('id')
     serializer_class = IngredientsSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
 
+    permission_classes = (CustomPermission,)
     queryset = Product.objects.all().order_by('-added_on')
     serializer_class = ProductSerializer
 
     def get_queryset(self, *args, **kwargs):
         queryset = super(ProductViewSet, self).get_queryset(*args, **kwargs)
+        user = self.request.user
 
         if user.is_anonymous or not user.is_staff:
             queryset = queryset.filter(is_active=True)
