419790db8cb69163648a89d6ce170756d796c1cf
shahrukh <shoaibtayyab121@gmail.com>
2020-11-03 06:58:32 +0530

    added functionality: customers will get only available prooduct

--- bakery_management/product/views.py
+++ bakery_management/product/views.py
@@ -1,4 +1,3 @@
-from rest_framework import generics
 from rest_framework import viewsets
 
 from product.serializers import ProductSerializer, IngredientsSerializer
@@ -13,5 +12,13 @@ class IngredientsViewSet(viewsets.ModelViewSet):
 
 class ProductViewSet(viewsets.ModelViewSet):
 
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by('-added_on')
     serializer_class = ProductSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super(ProductViewSet, self).get_queryset(*args, **kwargs)
+
+        if user.is_anonymous or not user.is_staff:
+            queryset = queryset.filter(is_active=True)
+
+        return queryset
