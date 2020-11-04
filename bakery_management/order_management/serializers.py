384fc776cc71efac408ef483cf0838577e2d1b0e
shahrukh <shoaibtayyab121@gmail.com>
2020-11-04 17:10:23 +0530

    updated existing models and created new model for Inventory

--- bakery_management/order_management/serializers.py
+++ bakery_management/order_management/serializers.py
@@ -1,32 +1,35 @@
 from rest_framework import serializers
 from order_management.models import Order, OrderDetails
 from product.serializers import ProductSerializer, IngredientsSerializer
+from product.models import Product
+
+class OrderProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderDetails
+        fields = ('__all__',)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ('id', 'quantity')
 
 
 class CreateOrderSerializer(serializers.ModelSerializer):
-    product_ids = serializers.ListField()
-
-    class Meta:
-        model = Order
-        fields = ('id', 'product_ids')
-
-    def create(self, validated_data):
-        product_ids = validated_data.pop('product_ids')
-        instance = super().create(validated_data)
-
-        for each in product_ids:
-            OrderDetails.objects.create(order=instance, product_id=each['id'], quantity=each['quantity'])
-
-        return instance
-
-        # validated_data.update({'customer': self.request.user})
-
-class OrderSerializer(serializers.ModelSerializer):
-    products = serializers.SerializerMethodField()
+    products = OrderSerializer(many=True)
 
     class Meta:
         model = Order
         fields = ('id', 'products')
 
-    def get_products(self, obj):
-        return ProductSerializer(obj.order_detail.all(), many=True).data
+    def create(self, validated_data):
+        products = validated_data.pop('products')
+        instance = super().create(validated_data)
+
+        for each in products:
+            OrderDetails.objects.create(order=instance, product_id=each['id'], quantity=each['quantity'])
+
+        return instance
+
