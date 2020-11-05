b0a87c98afe5ede455e905f7c40340751fc1d75d
shahrukh <shoaibtayyab121@gmail.com>
2020-11-05 17:37:28 +0530

    Applied check for checking available quantity before ordering products

--- bakery_management/order_management/serializers.py
+++ bakery_management/order_management/serializers.py
@@ -3,14 +3,8 @@ from order_management.models import Order, OrderDetails
 from product.serializers import ProductSerializer, IngredientsSerializer
 from product.models import Product
 
+
 class OrderProductSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = OrderDetails
-        fields = ('__all__',)
-
-
-class OrderDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = OrderDetails
@@ -18,12 +12,12 @@ class OrderDetailSerializer(serializers.ModelSerializer):
 
 
 class CreateOrderSerializer(serializers.ModelSerializer):
-    products = OrderDetailSerializer(many=True,write_only=True)
+    products = OrderProductSerializer(many=True, write_only=True)
     product_details = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ['products', 'product_details' ]
+        fields = ['id', 'products', 'product_details' ]
 
     def get_product_details(self, obj):
         return list(obj.order_detail.values())
@@ -32,11 +26,37 @@ class CreateOrderSerializer(serializers.ModelSerializer):
         products = validated_data.pop('products')
         instance = super().create(validated_data)
 
+        order_list = list()
         for product in products:
-            OrderDetails.objects.create(order=instance,
-                                        product=product['product'],
-                                        quantity=product['quantity']
+            quantity = product['quantity']
+            product_obj = product['product']
+
+            #finding total price for a product
+            ordered_price = (product_obj.selling_price) * quantity
+
+            order_list.append(OrderDetails(order=instance,
+                                        product=product_obj,
+                                        quantity=quantity,
+                                        ordered_price=ordered_price
+                                        )
                                     )
+            product_obj.available_quantity = product_obj.available_quantity - quantity
+            product_obj.save()
+
+        OrderDetails.objects.bulk_create(order_list)
 
         return instance
 
+
+    def validate_products(self, products):
+
+        for product in products:
+            available_quantity = product['product'].available_quantity
+            quantity_to_buy = product['quantity']
+            product_name = product['product'].name
+            if available_quantity < quantity_to_buy:
+                    error = "Only {} Products are available for {}".format(
+                                available_quantity, product_name)
+                    raise serializers.ValidationError(error)
+
+        return products
